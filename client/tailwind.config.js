7febeb2617c00ce7075a7c31b0ce2eb8f592d8b1
Romain Varra <romain.varra@hotmail.com>
2025-01-04 14:06:26 +0100

    feat: tailwind configuration done

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Les fichiers sources où Tailwind doit scanner les classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
