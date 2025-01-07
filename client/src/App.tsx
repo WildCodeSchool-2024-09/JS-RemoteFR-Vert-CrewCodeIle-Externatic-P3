7074060f444ee2ae2f5db3e73f1746a3a15c2dde
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2025-01-07 16:18:58 +0100

    feat : created Connection component HomePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
-import "./App.css";
 import "./index.css";
+import "./App.css";
+
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+	return (
+		<main>
+			<Outlet />
+		</main>
+	);
 }
 
 export default App;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import Connection from "../components/Connection";
+
+const HomePage = () => {
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Connection />
+		</div>
+	);
+};
+
+export default HomePage;
