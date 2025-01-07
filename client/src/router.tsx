13790ce3a89292057ecd7e6cdef81c23609f287c
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2025-01-07 19:44:07 +0100

    feat : create router and fix biome

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,14 +2,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
+  {
+    path: "/",
+    element: <App />,
+  },
 ]);
 
 const Router = () => {
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default Router;
