a73f92ed5a4a582ad0b4c5c4efc4a2a17e27717e
Mathilde Glet <you@example.com>
2025-01-08 19:11:49 +0100

    fix: add of good path for OffersResearch route page

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: "/",
+        path: "/OffersResearch",
         element: <OffersResearchPage />,
       },
     ],
