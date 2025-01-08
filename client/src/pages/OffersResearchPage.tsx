5d214fb8b609894df680eea24082dd88d14dc04f
Mathilde Glet <you@example.com>
2025-01-08 17:26:04 +0100

    feat: creation of OffersResearchPage's route

---
+++ client/src/pages/OffersResearchPage.tsx
@@ -0,0 +1,3 @@
+export default function OffersResearchPage() {
+  return <h1>OffersResearchPage</h1>;
+}
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import HomePage from "./pages/HomePage";
+import OffersResearchPage from "./pages/OffersResearchPage";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/",
+        element: <OffersResearchPage />,
       },
     ],
   },
