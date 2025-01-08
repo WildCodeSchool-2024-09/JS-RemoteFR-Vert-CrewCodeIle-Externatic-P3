8d5831b550f627e9623100b0a80b6088cd97f049
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2025-01-08 15:48:23 +0100

    feat : creation of component Main #1

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import About from "../components/About";
 import SignUp from "../components/SignUp";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <SignUp />
+      <About />
     </div>
   );
 };
