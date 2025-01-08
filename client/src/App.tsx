f0ef0b3318466b56acbc8bcb0c92766393e16ef3
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2025-01-08 09:35:57 +0100

    feat : creation of HomePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main>
+      <Outlet />
+    </main>
   );
 }
 
