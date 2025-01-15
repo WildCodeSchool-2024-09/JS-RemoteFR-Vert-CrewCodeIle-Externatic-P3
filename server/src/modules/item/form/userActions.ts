5ca5e4cf542dbb82f3edc502c43d62c203f47fa2
Romain Varra <romain.varra@hotmail.com>
2025-01-15 12:28:05 +0100

    feat: created browse and add const for userActions

---
+++ server/src/modules/item/form/userActions.ts
@@ -0,0 +1,41 @@
+import type { RequestHandler } from "express";
+
+import userRepository from "./userRepository";
+
+const browse: RequestHandler = async (req, res, next) => {
+  try {
+    // Fetch all categories
+    const users = await userRepository.readAll();
+
+    // Respond with the categories in JSON format
+    res.json(users);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+const add: RequestHandler = async (req, res, next) => {
+  try {
+    const newUser = {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: req.body.password,
+      adress: req.body.adress,
+      postal_code: req.body.postal_code,
+      city: req.body.city,
+      tel: req.body.tel,
+      is_active: req.body.is_active,
+      is_role: req.body.is_role,
+      role_id: req.body.role_id,
+    };
+    const insertId = await userRepository.create(newUser);
+
+    res.status(201).json({ insertId });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { browse };
