74084cbb7b17c6572b35278c1ea6b9c801b56922
ChristineV67 <christinevoinson@yahoo.com>
2025-01-16 14:40:24 +0100

    feat: update HomePage

--- client/src/components/OffersList.tsx
+++ client/src/components/OffersList.tsx
@@ -14,20 +14,26 @@ function OffersList() {
 
   return (
     <>
-    <h1 className="max-w-4xl mx-auto px-4 py-6  flex justify-around text-2xl">Nos offres en cours</h1>
-    
-    <section className="grid grid-cols-3 gap-4"> 
+      <h1 className="max-w-4xl mx-auto px-4 py-6  flex justify-around text-2xl">
+        Nos offres en cours
+      </h1>
+
+      <section className="grid grid-cols-3 gap-4">
         {dataOffers?.map((o) => (
-          <ul
-          key={o.id} 
-          className= " shadow-lg bg-[#851342] ">
-            <li className="text-white text-xl mb-2  font-medium " > {o.name} </li>
-           <li className=" text-white text-xl mb-2 font-medium  ">  {o.salaire}</li>
-           <li className="text-white text-xl mb-2 font-medium " >  {o.ville}</li>
-           <li className="text-white text-xl mb-2 font-medium " >  {o.id}</li>
+          <ul key={o.id} className=" shadow-lg bg-[#851342] ">
+            <li className="text-white text-xl mb-2  font-medium ">
+              {" "}
+              {o.name}{" "}
+            </li>
+            <li className=" text-white text-xl mb-2 font-medium  ">
+              {" "}
+              {o.salaire}
+            </li>
+            <li className="text-white text-xl mb-2 font-medium "> {o.ville}</li>
+            <li className="text-white text-xl mb-2 font-medium "> {o.id}</li>
           </ul>
         ))}
-  </section>
+      </section>
     </>
   );
 }
