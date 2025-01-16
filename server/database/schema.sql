bf332d25269c31f46ffa5192d0059c7668025be5
ChristineV67 <christinevoinson@yahoo.com>
2025-01-16 20:35:36 +0100

    feat: update dev

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,16 +1,1 @@
-CREATE TABLE tag (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  ville VARCHAR(100),
-  salaire VARCHAR(100)
-);
 
-INSERT INTO tag 
-VALUES
-(1,"Business Developer @SaaS - H/F","Lille","50 000 - 70 000 € par an "),
-(2,"Business Developer @SaaS - H/F","Lille","50 000 - 70 000 € par an "),
-(3,"Business Developer @SaaS - H/F","Lille","50 000 - 70 000 € par an "),
-(4,"Business Developer @SaaS - H/F","Lille","50 000 - 70 000 € par an "),
-(5,"Business Developer @SaaS - H/F","Lille","50 000 - 70 000 € par an "),
-(6,"Business Developer @SaaS - H/F","Lille","50 000 - 70 000 € par an "),
-(7,"toto","Lille","50 000 - 70 000 € par an ");
