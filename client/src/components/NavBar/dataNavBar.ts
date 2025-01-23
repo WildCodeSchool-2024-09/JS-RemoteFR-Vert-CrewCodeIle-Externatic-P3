be6be284372f86f0d5fbc54d3fd18b56004d5650
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2025-01-23 09:53:09 +0100

    feat : creation of navbar #44

---
+++ client/src/components/NavBar/dataNavBar.ts
@@ -0,0 +1,5 @@
+export const navigationLinks = [
+  { href: "/#qui-sommes-nous", label: "Qui sommes-nous ?" },
+  { href: "/#nos-offres", label: "Nos Offres" },
+  { href: "/#partenaires", label: "Entreprises partenaires" },
+];
