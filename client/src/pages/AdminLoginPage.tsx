1e58c47fb5d887a2da4fe807cb5751db20ca151f
Romain Varra <romain.varra@hotmail.com>
2025-02-17 12:17:21 +0100

    feat: redirection if candidate is not login for admin part

--- client/src/pages/AdminLoginPage.tsx
+++ client/src/pages/AdminLoginPage.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 import logo from "../assets/images/EXTERNATIC-LOGO-ORIGINAL-RVB.png";
 import Login from "../components/user/Login";
 import type { loginCompanyType } from "../lib/userForm.definitions";
@@ -22,12 +21,9 @@ const AdminLoginPage = () => {
       );
 
       if (response.ok) {
-        toast.success("Vous êtes bien connecté !");
         navigate("/admin/home");
       } else {
-        toast.error(
-          "Un problème est survenu lors de votre connexion ! Veuillez réessayer",
-        );
+        navigate("/");
       }
     } catch (err) {
       err;
